9db999a522139e949fb6aa3261c229ebd4284e49
maiko254 <bonyomichael406@gmail.com>
2023-11-12 08:39:47 +0000

    simple shell that interpretes command line that has only one word and no arguments

---
+++ mshell.c
@@ -0,0 +1,58 @@
+#include "sh.h"
+
+/**
+ * main - reads the command line and executes the program or prints
+ *        an error message if the executable cannot be found
+ *
+ * Return: Always 0
+ */
+extern char **environ;
+
+int main(void)
+{
+	char *lineptr;
+	size_t n;
+	pid_t child_pid;
+	int status;
+	struct stat st;
+	char **arr;
+	int len;
+
+	lineptr = NULL;
+	n = 0;
+
+	printf("#mshell$ ");
+	while (getline(&lineptr, &n, stdin) != EOF)
+	{
+		len = strlen(lineptr);
+		lineptr[len - 1] = '\0';
+			
+		if (stat(lineptr, &st) == 0)
+		{
+			child_pid = fork();
+			if (child_pid == -1)
+			{
+				perror("Error:");
+				exit(EXIT_FAILURE);
+			}
+
+			if (child_pid == 0)
+			{
+				arr = malloc(sizeof(char *) * 2);
+				arr[0] = lineptr;
+				arr[1] = NULL;
+				execve(arr[0], arr, environ);
+			}
+			else
+			{
+				wait(&status);
+			}
+		}
+		else
+			printf("File Not Found\n");
+		printf("#mshell$ ");
+	}
+
+	free(lineptr);
+	return (0);
+}
---
+++ sh.h
@@ -0,0 +1,12 @@
+#ifndef SH_H
+#define SH_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
